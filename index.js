864e4da22472487d6833bc5cb4721fa7bb4f6b1b
Aaron Lidman <aaronlidman@gmail.com>
2015-02-12 12:12:53 -0500

    Basic server

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log('please slug this repo');
+var hapi = require('hapi'),
+    pg = require('pg');
+
+var user = process.env.DBUsername;
+var password = process.env.DBPassword;
+var address = process.env.DBAddress;
+var database = process.env.Database;
+
+// on RDS, how do I set a security group?
+    // or whitelist this instance or something, somehow
+    // this must not happen in the main app, probably in install somewhere
+
+var conString = 'postgres://' +
+    user + ':' +
+    password + '@' +
+    address + '/' +
+    database;
+
+var server = new hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+server.route({
+    method: 'GET',
+    path:'/stuff',
+    handler: function(request, reply) {
+        console.log(request, reply);
+        reply('between the bars');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/status',
+    handler: function(request, reply) {
+        reply('ok');
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/csv',
+    handler: function(request, reply) {
+        reply('gogo');
+    }
+});
+
+// soooo, we need server side user auth
+    // in order to restrict uploads to specific users
+    // just use an env var for first pass?
+        // require a cooldown
+    // every POST must then
+
+// first pass, upload a csv
+    // retructure into k/v, hash, full object as string
+    // later we can pull special columns, latitude, longitude, _country
+        // what does the editor geocoder do? ffforest?
