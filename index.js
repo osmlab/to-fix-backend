fd6ee71f3a303b53828bf60e3e9c301e1ec30e35
Aaron Lidman <aaronlidman@gmail.com>
2015-03-17 18:33:35 -0400

    Add /track endpoint

--- index.js
+++ index.js
@@ -76,6 +76,26 @@ function track(table, time, attributes, callback) {
 }
 
 server.route({
+    method: 'GET',
+    path: '/track/{task}/{attr}',
+    handler: function(request, reply) {
+        // future, add range param
+        var table = request.params.task.replace(/[^a-zA-Z]+/g, '').toLowerCase();
+        var attr = request.params.attr.split(':');
+            // hstore key:value
+        var query = 'SELECT time as unixtime, hstore_to_json_loose(attributes) as attributes from ' + table + '_stats where attributes->$1=$2 order by time ASC;';
+
+        client.query(query, [attr[0], attr[1]], function(err, results) {
+            if (err) return console.log(err);
+            reply({
+                updated: Math.round(+new Date()/1000),
+                data: results.rows
+            });
+        });
+    }
+});
+
+server.route({
     method: 'POST',
     path: '/error/{error}',
     handler: function(request, reply) {
