0ac93b7c841e804c7a02e59f95a2e81e0ed96030
Rub21 <ruben@mapbox.com>
2015-07-18 22:35:30 -0500

    fix  for each format task

---
+++ query/removetables.sql
@@ -0,0 +1,60 @@
+﻿--task tables--
+
+--deadendoneway
+--deadendoneway_stats
+--disconnectednepal
+--disconnectednepal_stats
+--highwayfootpath
+--highwayfootpath_stats
+--highwayhighway
+--highwayhighway_stats
+--highwayriverbank
+--highwayriverbank_stats
+--impossibleangle
+--impossibleangle_stats
+--loopings
+--loopings_stats
+--mispelledtags
+--mispelledtags_stats
+--mixedlayer
+--mixedlayer_stats
+--nonclosedways
+--nonclosedways_stats
+--nycbuildingoverlaps
+--nycbuildingoverlaps_stats
+--rk
+--rk_stats
+--strangelayer
+--strangelayer_stats
+--tigerdelta
+--tigerdelta_stats
+--unconnectedmajor
+--unconnectedmajor_stats
+--unconnectedminor
+--unconnectedminor_stats
+--usabuildingoverlaps
+--usabuildingoverlaps_stats
+
+
+-- Remove tables , we dont use those
+
+
+DROP TABLE nycadreessspaciala_stats;
+DROP TABLE nycoverlappingbuildings;
+DROP TABLE nycoverlappingbuildings_stats;
+DROP TABLE nycoverlappingbuildingsversion;
+DROP TABLE nycoverlappingbuildingsversion_stats;
+DROP TABLE nycspeciala;
+DROP TABLE nycspeciala_stats;
+DROP TABLE nycspecialb;
+DROP TABLE nycspecialb_stats;
+DROP TABLE temp_nycaddressspecialcoa;
+DROP TABLE temp_nycaddressspecialcob;
+DROP TABLE temp_tigerdelta;
+DROP TABLE temp_unconnectedmajor;
+DROP TABLE test;
+DROP TABLE testruben;
+DROP TABLE testruben_stats;
+DROP TABLE test_stats;
+DROP TABLE usabuildingsoverlap;
+DROP TABLE usabuildingsoverlap_stats;
