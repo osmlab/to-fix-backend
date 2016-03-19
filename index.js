3f00e71f393a9c41328eb723bebeddd1d04972fd
Rub21 <ruben@mapbox.com>
2016-03-19 13:04:26 -0500

    UploadPath default

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var user = process.env.DBUsername || 'postgres';
 var password = process.env.DBPassword || '';
 var address = process.env.DBAddress || 'localhost';
 var database = process.env.Database || 'tofix';
-var path = process.env.UploadPath;
+var path = process.env.UploadPath || '/tmp';
 // short term, to prevent the need from building out user authentication until later
 var uploadPassword = process.env.UploadPassword;
 if (!path) return console.log('env variable UploadPath must be set');
