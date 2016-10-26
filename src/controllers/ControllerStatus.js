ccbcce79148adc1f2dd1883899f4521813207b7b
Rub21 <ruben@mapbox.com>
2016-10-26 15:46:33 +0530

    testing http-aws-es

--- src/controllers/ControllerStatus.js
+++ src/controllers/ControllerStatus.js
@@ -1,14 +1,16 @@
 'use strict';
 var os = require('os');
 var client = require('./../utils/connection.js');
-
 module.exports.status = function(request, reply) {
   client.cluster.health({}, function(err, response) {
-    if (err) reply('error').code(500);
-    reply({
-      status: 'a ok',
-      database: response,
-      server: os.platform()
-    });
+    if (err) {
+      reply(err);
+    } else {
+      reply({
+        status: 'a ok',
+        database: response,
+        server: os.platform()
+      });
+    }
   });
 };
--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,7 +1,23 @@
 'use strict';
+// var elasticsearch = require('elasticsearch');
+// var client = new elasticsearch.Client({
+//   host: process.env.ElasticHost || 'localhost:9200',
+//   log: 'trace'
+// });
+// module.exports = client;
+
 var elasticsearch = require('elasticsearch');
+var AwsEsConnector = require('http-aws-es');
+var amazonES = {
+  region: 'us-east-1',
+  accessKey: process.env.AWS_ACCESS_KEY_ID,
+  secretKey: process.env.AWS_SECRET_ACCESS_KEY
+};
 var client = new elasticsearch.Client({
-  host: process.env.ElasticHost || 'localhost:9200',
-  log: 'trace'
+  hosts: process.env.ElasticHost || 'localhost:9200',
+  log: 'trace',
+  connectionClass: AwsEsConnector,
+  amazonES: amazonES
 });
+
 module.exports = client;
