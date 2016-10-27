4cef640199e40bead2d592c5c98726941ee106c8
Rub21 <ruben@mapbox.com>
2016-10-27 13:58:32 +0530

    creds refresh

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -4,15 +4,18 @@ var elasticsearch = require('elasticsearch');
 var AwsEsConnector = require('http-aws-es');
 var creds = new AWS.ECSCredentials();
 creds.get();
-var amazonES = {
-  region: 'us-east-1',
-  credentials: creds.refresh()
-};
-console.log(amazonES);
-var client = new elasticsearch.Client({
-  host: process.env.ElasticHost || 'localhost:9200',
-  log: 'trace',
-  connectionClass: AwsEsConnector,
-  amazonES: amazonES
+var client;
+creds.refresh(function(err) {
+  if (err) throw err;
+  var amazonES = {
+    region: 'us-east-1',
+    credentials: creds
+  };
+  client = new elasticsearch.Client({
+    host: process.env.ElasticHost || 'localhost:9200',
+    log: 'trace',
+    connectionClass: AwsEsConnector,
+    amazonES: amazonES
+  });
 });
 module.exports = client;
