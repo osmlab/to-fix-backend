a8b777ab34b58686c939be0fbec2d6233c1a63c2
Rub21 <ruben@mapbox.com>
2016-10-27 10:30:20 +0530

    for test

--- src/controllers/ControllerStatus.js
+++ src/controllers/ControllerStatus.js
@@ -2,15 +2,15 @@
 var os = require('os');
 var client = require('./../utils/connection.js');
 module.exports.status = function(request, reply) {
-  client.cluster.health({}, function(err, response) {
-    if (err) {
-      reply(err);
-    } else {
-      reply({
-        status: 'a ok',
-        database: response,
-        server: os.platform()
-      });
-    }
+  // client.cluster.health({}, function(err, response) {
+  //   if (err) {
+  //     reply(err);
+  //   } else {
+  reply({
+    status: 'a ok',
+    // database: response,
+    server: os.platform()
   });
+  //   }
+  // });
 };
