2bf70d91e4cd3b1e82ddc6be7242114098e7d1e7
Rub21 <ruben@mapbox.com>
2016-10-27 09:55:45 +0530

    ecs credentias

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,23 +1,18 @@
 'use strict';
-// var elasticsearch = require('elasticsearch');
-// var client = new elasticsearch.Client({
-//   host: process.env.ElasticHost || 'localhost:9200',
-//   log: 'trace'
-// });
-// module.exports = client;
-
+var AWS = require('aws-sdk');
 var elasticsearch = require('elasticsearch');
 var AwsEsConnector = require('http-aws-es');
+var creds = new AWS.ECSCredentials();
+creds.get();
 var amazonES = {
   region: 'us-east-1',
-  accessKey: process.env.AWS_ACCESS_KEY_ID,
-  secretKey: process.env.AWS_SECRET_ACCESS_KEY
+  credentials: creds.refresh()
 };
+console.log(amazonES);
 var client = new elasticsearch.Client({
-  hosts: process.env.ElasticHost || 'localhost:9200',
+  host: process.env.ElasticHost || 'localhost:9200',
   log: 'trace',
   connectionClass: AwsEsConnector,
   amazonES: amazonES
 });
-
 module.exports = client;
