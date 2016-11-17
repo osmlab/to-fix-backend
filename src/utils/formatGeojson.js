f04010b04ae84e2e6101023d299a48e00409b247
Rub21 <ruben@mapbox.com>
2016-11-17 18:55:47 -0500

    add testing for feature

--- src/utils/formatGeojson.js
+++ src/utils/formatGeojson.js
@@ -7,15 +7,64 @@ var path = require('path');
 var sha1 = require('sha1');
 var _ = require('underscore');
 var folder = os.tmpDir();
-//These are the most usual id which come in geojson files
+//These are the most usual ids which come in geojson files.
 var ids = ['id', '_id', '_fromWay', '_toWay'];
 
+module.exports = {
+  formatGeojson,
+  formatFeature
+};
+
 function handleData(data, file, es) {
+  data = formatFeature(data);
+  var row = `${JSON.stringify(data)}\n`;
+  fs.appendFile(file, row, function(err) {
+    if (err) console.log(err);
+    es.resume();
+  });
+}
+
+/**
+ * Stream a GeoJSON file to count the number of rows
+ * @param {string} geojsonFile path to a local GeoJSON file
+ * @param {object} task metadata
+ * @param {function} cb success callback passing updated task as parameter
+ * @returns {object} task with `items` property
+ */
+function formatGeojson(geojsonFile, task, cb) {
+  var numRows = 0;
+  var file = path.join(folder, task.idtask);
+  fs.writeFile(file, '');
+  var fileStream = fs.createReadStream(geojsonFile, {
+    encoding: 'utf8'
+  });
+  fileStream.pipe(JSONStream.parse('features.*')).pipe(eventStream.through(function(data) {
+    numRows++;
+    this.pause();
+    handleData(data, file, this);
+    return data;
+  }, function end() {
+    task.value.stats[task.value.stats.length - 1].date = task.value.updated;
+    task.value.stats[task.value.stats.length - 1].items = numRows;
+    cb(task);
+    this.emit('end');
+  }));
+}
+
+/**
+ * Format the data
+ * - add _key, hash of the feature
+ * - add _time, current time
+ * - add _osmId , which is the id of osm object
+ * - add _osmType, which is the type of osm object
+ * @param {object} a feature of geojson
+ * @returns {object} a feature which will be a item in ES
+ */
+function formatFeature(data) {
   var hash = sha1(JSON.stringify(data));
   var time = Math.round((new Date()).getTime() / 1000);
   data.properties._key = hash;
   data.properties._time = time;
-  //for while lest set up the type of osm object, here
   if (data.geometry.type === 'MultiPoint' || data.geometry.type === 'LineString' || data.geometry.type === 'MultiLineString' || data.geometry.type === 'Polygon' || data.geometry.type === 'MultiPolygon') {
     data.properties._osmType = 'way';
   } else if (data.geometry.type === 'Point') {
@@ -38,36 +87,5 @@ function handleData(data, file, es) {
       }
     }
   }
-  var row = `${JSON.stringify(data)}\n`;
-  fs.appendFile(file, row, function(err) {
-    if (err) console.log(err);
-    es.resume();
-  });
+  return data;
 }
-
-/**
- * Stream a GeoJSON file to count the number of rows
- * @param {string} geojsonFile path to a local GeoJSON file
- * @param {object} task metadata
- * @param {function} cb success callback passing updated task as parameter
- * @returns {object} task with `items` property
- */
-module.exports.formatGeojson = function(geojsonFile, task, cb) {
-  var numRows = 0;
-  var file = path.join(folder, task.idtask);
-  fs.writeFile(file, '');
-  var fileStream = fs.createReadStream(geojsonFile, {
-    encoding: 'utf8'
-  });
-  fileStream.pipe(JSONStream.parse('features.*')).pipe(eventStream.through(function(data) {
-    numRows++;
-    this.pause();
-    handleData(data, file, this);
-    return data;
-  }, function end() {
-    task.value.stats[task.value.stats.length - 1].date = task.value.updated;
-    task.value.stats[task.value.stats.length - 1].items = numRows;
-    cb(task);
-    this.emit('end');
-  }));
-};
--- test/formatGeojson.test.js
+++ test/formatGeojson.test.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var formatGeojson = require('../src/utils/formatGeojson').formatGeojson;
+var formatFeature = require('../src/utils/formatGeojson').formatFeature;
 var os = require('os');
+var sha1 = require('sha1');
 var test = require('tape');
 
 var fixturesPath = __dirname + '/fixtures/';
@@ -9,7 +11,8 @@ var loadFixture = filename => toJSON(fs.readFileSync(fixturesPath + filename));
 
 var fixtures = {
   strangeLayer: loadFixture('strangelayerpql.json'),
-  simpleGeoJSON: loadFixture('simple.geojson')
+  simpleGeoJSON: loadFixture('simple.geojson'),
+  simpleFeature: loadFixture('feature.json')
 };
 
 test('it converts a GeoJSON file into line-delimited features', function(t) {
@@ -27,4 +30,13 @@ test('it stores line-delimited features in temp file', function(t) {
     t.true(fs.existsSync(tempfile), `finds a temp file at ${tempfile}`);
     t.end();
   });
+});
+
+test('it format the feature to be an item', function(t) {
+  var data = formatFeature(fixtures.simpleFeature);
+  var _key = '308195d6bec09f2514082e7feaeb836939789d57';
+  var _osmType = 'node';
+  t.equal(data.properties._key, _key, 'same hash for feature');
+  t.equal(data.properties._osmType, _osmType, 'same osm obj type');
+  t.end();
 });
