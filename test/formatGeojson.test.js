9f27258a5aedaa6d8acc69c1dbcdd6a96c449ba2
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-11-17 15:49:13 -0500

    Setup testing with tape

---
+++ test/formatGeojson.test.js
@@ -0,0 +1,10 @@
+
+var formatGeojson = require('../src/utils/formatGeojson');
+var test = require('tape');
+
+var fixtures = {};
+
+test('it converts a GeoJSON file into line-delimited features', function(t) {
+  t.fail('just pass');
+  t.end();
+});
