f0a5a756cc8592e3fadd7c4407bf50300307a7e5
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-11-29 12:29:10 -0500

    Test endpoint using server injection

---
+++ test/routesStatus.test.js
@@ -0,0 +1,18 @@
+var server = require('../index');
+var test = require('tape');
+
+test('GET /', function (t) {
+  server.inject('/', (res) => {
+    t.equal(res.statusCode, 200, 'HTTP 200 OK');
+    t.end();
+  });
+
+});
+
+test('teardown', function (t) {
+  server.stop({ timeout: 1000 }, (err) => {
+    console.log('shutting down');
+    server.emit('stop');
+    t.end();
+  });
+});
