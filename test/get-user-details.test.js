f41b0282923503d7d688c80da4edb70e428ed59a
Sanjay Bhangar <sanjay@mapbox.com>
2017-10-06 11:39:31 +0530

    use nock to mock requests to the osm api

--- test/get-user-details.test.js
+++ test/get-user-details.test.js
@@ -1,18 +1,12 @@
-// Right now get-user-details does not interact with the DB,
-// so we can use vanilla `tape`.
-
-const tape = require('tape');
+const test = require('./lib/test');
 const getUserDetails = require('../lib/get-user-details');
 
-tape('test get user details', assert => {
+test('test get user details', [], assert => {
   const oauthTokenSecret = 'faketokensecret';
   const oauthToken = 'faketoken';
 
-  //TODO: mock the OSM API for tests
   getUserDetails(oauthToken, oauthTokenSecret).then(user => {
-    //TODO: assert response is as expected from our mocks
-    assert.deepEqual(user, {});
-
+    assert.equal(user.display_name, 'userone', 'user xml parsed as expected');
     assert.end();
   });
 });
