e56eaa640aae90c0bd042874821a8d38f27a0735
Emily DuBois <emilymdubois@gmail.com>
2017-10-17 17:23:57 -0700

    Adds starter project-level CRUD endpoints

--- routes/tag.js
+++ routes/tag.js
@@ -1,24 +1,112 @@
+const db = require('../database/db');
+const ItemTags = db.ItemTags;
+const ErrorHTTP = require('mapbox-error').ErrorHTTP;
+
 module.exports = {
+  /* Project-level operations */
+  getProjectTags: getProjectTags,
+  createProjectTag: createProjectTag,
+  getProjectTag: getProjectTag,
+  updateProjectTag: updateProjectTag,
+  deleteProjectTag: deleteProjectTag,
+  /* Item-level operations */
   getItemTags: getItemTags,
-  getItemTag: getItemTag,
   createItemTag: createItemTag,
-  updateItemTag: updateItemTag,
   deleteItemTag: deleteItemTag
 };
 
+/**
+ */
+function getProjectTags(req, res, next) {
+  ItemTags.findAll({
+    where: {
+      project_id: req.params.project
+    }
+  })
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(next);
+}
+
+/**
+ */
+function createProjectTag(req, res, next) {
+  if (!req.body) return next(new ErrorHTTP('request.body is required', 422));
+  if (!req.body.name)
+    return next(new ErrorHTTP('request.body.name is required', 422));
+  let values = { name: req.body.name, project_id: req.params.project };
+  if (req.body.metadata) values.metadata = req.body.metadata;
+  ItemTags.create(values)
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(next);
+}
+
+/**
+ */
+function getProjectTag(req, res, next) {
+  ItemTags.findAll({
+    where: {
+      id: req.params.tag,
+      project_id: req.params.project
+    }
+  })
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(next);
+}
+
+/**
+ */
+function updateProjectTag(req, res, next) {
+  if (!req.body) return next(new ErrorHTTP('request.body is required', 422));
+  if (!req.body.name)
+    return next(new ErrorHTTP('request.body.name is required', 422));
+
+  ItemTags.update(
+    {
+      name: req.body.name
+    },
+    {
+      where: {
+        id: req.params.tag,
+        project_id: req.params.project
+      }
+    }
+  )
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(next);
+}
+
+/**
+ */
+function deleteProjectTag(req, res, next) {
+  ItemTags.destroy({
+    where: {
+      id: req.params.tag,
+      project_id: req.params.project
+    }
+  })
+    .then(function(data) {
+      res.json(data);
+    })
+    .catch(next);
+}
+
+/**
+ */
 function getItemTags(req, res, next) {
   return next();
 }
 
-function getItemTag(req, res, next) {
-  return next();
-}
-
+/**
+ */
 function createItemTag(req, res, next) {
-  return next();
-}
-
-function updateItemTag(req, res, next) {
   return next();
 }
 
