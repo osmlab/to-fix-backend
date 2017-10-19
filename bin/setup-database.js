544bd59a133e7825f12fd01dfe1c1f7e73321f00
Sanjay Bhangar <sanjay@mapbox.com>
2017-10-19 13:33:01 -0700

    use database as session store, fixes #152

--- bin/setup-database.js
+++ bin/setup-database.js
@@ -1,5 +1,7 @@
 var db = require('../database/index');
 const logger = require('fastlog')('setup-database');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 db
   .query('SELECT * FROM public.geometry_columns')
@@ -12,8 +14,15 @@ db
     return db.sync({ force: process.env.DROP === 'true', logging: false });
   })
   .then(function() {
+    const sessionStore = new SequelizeStore({
+      db: db
+    });
+    return sessionStore.sync();
+  })
+  .then(function() {
     logger.info('database is setup');
     db.close();
+    process.exit(0);
   })
   .catch(function(err) {
     logger.error(err.message);
