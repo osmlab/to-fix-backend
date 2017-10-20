18acfe2745e0684c2fba139727778837507fb158
Kushan Joshi <0o3ko0@gmail.com>
2017-10-20 16:24:07 -0700

    add user enpoint

---
+++ test/user.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const test = require('./lib/test');
+
+test('GET / - db connection success', [], (assert, token) => {
+  assert.app
+    .get('/user')
+    .set('authorization', token)
+    .expect(200, (err, res) => {
+      if (err) return assert.end(err);
+      assert.deepEqual(res.body, { id: 123, username: 'test-user' });
+      assert.end();
+    });
+});
