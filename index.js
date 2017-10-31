709e3f89b75f3496d7bf074828317dee215dd2ef
Eli Fitch <eli.fitch.prof@gmail.com>
2017-10-31 15:19:56 -0400

    logging abstraction

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const server = require('./lib/server');
-const logger = require('fastlog')('to-fix-backend');
+const logDriver = require('./lib/log-driver')('index');
 
 process.on('uncaughtException', function(err) {
-  logger.fatal(err);
+  logDriver.error(err);
   process.exit(1);
 });
 
 const port = process.env.PORT || 8889;
 server.listen(port, function(err) {
   if (err) throw err;
-  logger.info('listing on port: ' + port);
+  logDriver.info('listing on port: ' + port);
 });
