28a69efb512b03051410b2fdf79227a3ae92bd32
Eli Fitch <eli.fitch.prof@gmail.com>
2017-11-07 16:43:30 -0500

    working kinesis logging

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const server = require('./lib/server');
 const logDriver = require('./lib/log-driver')('index');
 
 process.on('uncaughtException', function(err) {
-  logDriver.error(err);
+  logDriver.error(err, { exportLog: true, event: 'crash' });
   process.exit(1);
 });
 
 const port = process.env.PORT || 8889;
 server.listen(port, function(err) {
   if (err) throw err;
-  logDriver.info('listing on port: ' + port);
+  logDriver.info('listing on port: ' + port, {
+    exportLog: true,
+    event: 'AppStart'
+  });
 });
