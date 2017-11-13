6f440340515d4018089c22e597fdf6185da26324
Sanjay Bhangar <sanjay@mapbox.com>
2017-11-13 15:05:17 +0530

    script to add quadkey info to existing items

---
+++ bin/add-quadkey-to-items.js
@@ -0,0 +1,36 @@
+const getQuadkeyForPoint = require('../lib/helper/get-quadkey-for-point');
+const db = require('../database/index');
+const Item = db.Item;
+const pMap = require('p-map');
+
+Item.findAll({
+  where: {
+    quadkey: null
+  }
+})
+  .then(items => {
+    return pMap(items, updateQuadkey, { concurrency: 50 });
+  })
+  .then(() => {
+    console.log('all items updated'); // eslint-disable-line no-console
+    return;
+  })
+  .catch(err => {
+    console.log('ERROR: ', err); // eslint-disable-line no-console
+    return;
+  });
+
+function updateQuadkey(item) {
+  const quadkey = getQuadkeyForPoint(item.pin);
+  return Item.update(
+    {
+      quadkey: quadkey
+    },
+    {
+      where: {
+        id: item.id,
+        project_id: item.project_id
+      }
+    }
+  );
+}
